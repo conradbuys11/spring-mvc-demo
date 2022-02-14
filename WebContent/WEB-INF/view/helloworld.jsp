7591dac1a08ccaa8aa872fb0b9dde8f7e702ede6
conradbuys11 <conradbuys11@gmail.com>
2022-02-14 17:20:03 -0500

    completed exercise on HttpServletRequest and Model

--- WebContent/WEB-INF/view/helloworld.jsp
+++ WebContent/WEB-INF/view/helloworld.jsp
@@ -7,7 +7,10 @@
 <title>Insert title here</title>
 </head>
 <body>
-	Hello World of Spring!<br><br>
+	Hello World of Spring!
+	<br><br>
 	Student name: ${param.studentName}
+	<br><br>
+	The message: ${message}
 </body>
 </html>
--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -1,6 +1,9 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +18,24 @@ public class HelloWorldController {
 	//process form method
 	@RequestMapping("/processForm")
 	public String processForm() {
+		return "helloworld";
+	}
+	
+	//read form data and add to model
+	@RequestMapping("/processFormVersionTwo")
+	public String letsShoutDude(HttpServletRequest request, Model model) {
+		//read request parameter from form
+		String theName = request.getParameter("studentName");
+		
+		//convert data into all uppercase
+		theName = theName.toUpperCase();
+		
+		//create message
+		String result = "Yo! " + theName;
+		
+		//add message to model
+		model.addAttribute("message", result);
+		
 		return "helloworld";
 	}
 	
