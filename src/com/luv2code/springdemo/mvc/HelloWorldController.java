27db43c945e1d78180574b0949cfcb6a04f4f738
conradbuys11 <conradbuys11@gmail.com>
2022-02-16 15:52:46 -0500

    requestmapping & relative requestmapping

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
 	//show form method
@@ -32,6 +34,21 @@ public class HelloWorldController {
 		
 		//create message
 		String result = "Yo! " + theName;
+		
+		//add message to model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
+	
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+		
+		//convert data into all uppercase
+		theName = theName.toUpperCase();
+		
+		//create message
+		String result = "Hey " + theName + "!";
 		
 		//add message to model
 		model.addAttribute("message", result);
